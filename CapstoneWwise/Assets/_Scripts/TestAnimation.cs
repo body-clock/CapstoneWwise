1f013533d1396066e16253527bf41186cea4b52a
Mattias Lambert <mattiasl380@gmail.com>
2019-03-29 13:45:00 -0400

    Animation Test Scene

---
+++ CapstoneWwise/Assets/_Scripts/TestAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimation : MonoBehaviour
+{
+
+
+    public Animator cabinetAnimator;
+
+
+    void Start()
+    {
+        cabinetAnimator.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(startAnimation());
+        }
+
+       
+        
+    }
+
+    IEnumerator startAnimation()
+    {
+        yield return new WaitForSeconds(2f);
+        cabinetAnimator.gameObject.SetActive(true);
+        cabinetAnimator.SetBool("constructing", true);
+    }
+}

cb2c4f2404a6fb43023066eb25f428c35b3f8399
lil_blizzard <plp248@nyu.edu>
2019-04-07 23:32:55 -0400

    sounds for transition walking

--- CapstoneWwise/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ CapstoneWwise/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -26,6 +26,8 @@ namespace AK
         static const AkUniqueID PLAY_OCEAN_EVENT = 2984240761U;
         static const AkUniqueID PLAY_OCEANSPLASH_EVENT = 2977236588U;
         static const AkUniqueID PLAY_PILLAR_AUDIO_EVENT = 2025220688U;
+        static const AkUniqueID PLAY_TRANSITION_WALKING_EVENT = 3321450650U;
+        static const AkUniqueID STOP_TRANSITION_WALKING_EVENT = 3274795716U;
     } // namespace EVENTS
 
     namespace BANKS
@@ -41,6 +43,7 @@ namespace AK
         static const AkUniqueID LOOM2 = 7184428U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID TRANSITION = 1865857008U;
     } // namespace BUSSES
 
     namespace AUX_BUSSES
@@ -49,6 +52,7 @@ namespace AK
         static const AkUniqueID LOOM1_CANDLEVERB = 475597662U;
         static const AkUniqueID LOOM1_WALKDECAY = 511694909U;
         static const AkUniqueID LOOM2_WALKDECAY = 3240738094U;
+        static const AkUniqueID TRANSITION_AUX_BUS = 911329060U;
     } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
